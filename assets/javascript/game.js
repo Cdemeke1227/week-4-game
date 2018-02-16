6024c9cad379bee3da2a1f48fc7e4628c15af5de
Christopher Demeke <christopherdemeke@dmk.attlocal.net>
2018-02-15 20:52:17 -0600

    Updates to crystal collector for proper game play

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,10 @@
 
-// Pseudo Code
-
-
-
-
-// compare user total to crystal number, add logic{}
-// *The player wins if their total score matches the random number from the beginning of the game.
-//*Track number of winns and losses
-// *The player loses if their score goes above the random number.
-
-// based on comparison  logic, the game will restart by calling each function to update the crystal and user numbers
-$(document).ready(function () {
-
-
-
+// Crystal Collector Game
+// ==============================================
+$(function () {
+    // ==============================================
+    // Variables
+    // ==============================================
     let computerNumber;
     let computerNumberMax = 120;
     let computerNumberMin = 19;
@@ -26,47 +17,56 @@ $(document).ready(function () {
     let crystal4;
     let cyrstalNumber
     let num;
-
     let wins = 0;
     let loss = 0;
-
+    // ==============================================
+    // Functions
+    // ==============================================
+    // Generates Random Integer between max and min
     let getRandomNumber = (max, min) => {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+    // Will update the overall wing loss record
     let updateWinRecord = (wins, loss) => {
-            $('#winCount').text("Wins: " + wins);
-            $('#lossCount').text("Losses: " + loss);
+        $('#winCount').text("Wins: " + wins);
+        $('#lossCount').text("Losses: " + loss);
     }
+    // This will reset all the random numbes and total score
     let resetGame = () => {
         computerNumber = getRandomNumber(computerNumberMax, computerNumberMin);
+        crystal1 = getRandomNumber(crystalNumberMax, cyrstalNumberMin);
+        crystal2 = getRandomNumber(crystalNumberMax, cyrstalNumberMin);
+        crystal3 = getRandomNumber(crystalNumberMax, cyrstalNumberMin);
+        crystal4 = getRandomNumber(crystalNumberMax, cyrstalNumberMin);
         userScore = 0;
         $("#compNumber").text(computerNumber);
         $("#totalCount").text(userScore);
     }
     let crystalNumber = (crystalNum, num) => {
-        crystalNum = getRandomNumber(crystalNumberMax, cyrstalNumberMin);
+        // crystalNum = getRandomNumber(crystalNumberMax, cyrstalNumberMin);
         userScore = crystalNum + userScore;// when crystal is clicked it will add it value total
-        $('#crystalNumber').text(crystalNum);
-        // $('#gem' + num).text(crystalNum);
+        $('#gem' + num).text(crystalNum); // testing case to show gem number on tile
         $('#totalCount').text(userScore);
         resultCheck(computerNumber, userScore);
     }
     let resultCheck = (computerNumber, userScore) => {
         if (computerNumber === userScore) {
             wins++;
-           updateWinRecord(wins,loss);
+            updateWinRecord(wins, loss);
             resetGame();
 
         }
         else {
             if (computerNumber < userScore) {
                 loss++;
-                updateWinRecord(wins,loss);
+                updateWinRecord(wins, loss);
                 resetGame();
             }
         }
     }
-
+    // ==============================================
+    // Game Start
+    // ==============================================
     resetGame();
     updateWinRecord(wins, loss);
     // Each of the four crystals will have a rand number between 1 -12
@@ -82,12 +82,4 @@ $(document).ready(function () {
     $('#gem4').on("click", function () {
         crystalNumber(crystal4, '4');
     });
-
-
-
-
-
-
-
-
 });
